233bee4bc91069157a3bfac3cc550b54e42cc3f3
kunnoh <alvindereba@gmail.com>
2024-04-04 16:54:34 +0300

    more on reamde

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
-# deploy web server on AWS
+# Provision and configure an AWS EC2 instance using Terraform & Ansible
 
 ## Prerequsistes
+- AWS account
+- AWS CLI
+- Terraform
+- Ansible
+- Text editor(VSCode)
 
+## Create new user in AWS and configure AWS CLI
+One you create a new user on AWS, you should create a new user to play with. Don't use root account when it's not required. Create a new IAM user that has admin access.
 
-
-*Task*:
+*Done*:
 1. Create vpc
 2. Create Internet Gateway
 3. Create Custom Route Table
 4. Create a Subnet
 5. Associate subnet with Route Table
 6. Create Security Group to allow port 22,80,443
+
+*To do*:
 7. Create a network interface with an ip in the subnet that was created in step 4
 8. Assign an elastic IP to the network interface created in step 7
 9. Create debian server and install/enable apache2,nginx
 
 
-## References
-- [Terraform aws_pc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc)
+## Reference
+- [Terraform aws_vpc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc)
 
 - [Terraform aws_internet_gateway](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway)
 
@@ -25,3 +33,8 @@
 
 - [Terraform aws_security_group](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group)
 
+- [Terraform aws_subnet](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet)
+
+- [Terreform aws_route_table_association](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association)
+
+- []()
--- main.tf
+++ main.tf
@@ -15,47 +15,106 @@ provider "aws" {
 }
 
 # 1. Create vpc
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "Vpn-Vpc" {
+  cidr_block = "100.68.0.0/18"
   enable_dns_hostnames = true
   tags = {
-    Name = "dev-vpc"
+    Name = "Vpn-Vpc"
   }
 }
 
 # 2. Create Internet Gateway
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.dev-vpc.id
+resource "aws_internet_gateway" "Vpn-GW" {
+  vpc_id = aws_vpc.Vpn-Vpc.id
+  tags = {
+    Name = "Vpn-GW"
+  }
 }
 
 # 3. Create Custom Route Table
-resource "aws_route" "name" {
-  
+resource "aws_route_table" "Vpn-Routing-Table" {
+  vpc_id = aws_vpc.Vpn-Vpc.id
+  route {
+    cidr_block = "100.68.0.0/18"
+    gateway_id = aws_internet_gateway.Vpn-GW.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.Vpn-GW.id
+  }
+  tags = {
+    Name = "Vpn-Routing-Tabble"
+  }
 }
 
 # 4. Create a Subnet
-resource "aws_subnet" "dev-subnet" {
-  vpc_id     = aws_vpc.dev-vpc.id
-  cidr_block = aws_vpc.dev-vpc.cidr_block
-  availability_zone = "${data.aws_region.current.name}a"
-  
+resource "aws_subnet" "Vpn-Subnet" {
+  vpc_id     = aws_vpc.Vpn-Vpc.id
+  cidr_block = aws_vpc.Vpn-Vpc.cidr_block
+  availability_zone = "${data.aws_region.current.name}"
   tags = {
-    Name = "dev-subnet"
+    Name = "Vpn-Subnet"
   }
 }
 
 # 5. Associate subnet with Route Table
+resource "aws_route_table_association" "Vpn-Subnet_Association" {
+  subnet_id      = aws_subnet.example.id
+  route_table_id = aws_route_table.Vpn-Routing-Table.id
+}
+
 # 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_tls" {
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.Vpn-Vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.Vpn-Vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.Vpn-Vpc.ipv6_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.Vpn-Vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.Vpn-Vpc.ipv6_cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
 # 7. Create a network interface with an ip in the subnet that was created in step 4
 # 8. Assign an elastic IP to the network interface created in step 7
-# 9. Create Ubuntu server and install/enable apache2
 
+# 9. Create debian server and install/enable nginx
+resource "aws_instance" "vpn-server" {
+  ami = "ami-0588c11374527e516"
+  instance_type = "t2.micro"
 
-# resource "aws_instance" "vpn-server" {
-#     ami = "ami-0588c11374527e516"
-#     instance_type = "t2.micro"
-
-#     tags = {
-#       Name = "Vpn Server"
-#     }
-# }
+  tags = {
+    Name = "Vpn Server"
+  }
+    depends_on = [aws_internet_gateway.Vpn-GW]
+}
